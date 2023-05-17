70e2845534b210d578e941d1a2247a3e044c1779
biminkoju <biminkoju@gmail.com>
2023-05-17 19:19:42 +0545

    added assets and a icon for window

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
+#importing of modules and shit
 import pygame,sys
+
 #pygame setup
-clock=pygame.time.Clock()                           #game clock
 pygame.init()
-running=True
-WindowSize=(400,400)
+
+#image loading shit
+WindowLogo=pygame.image.load('./assets/1.png')
+playerimage=pygame.image.load('./assets/player.png')
+
+#initializations
+clock=pygame.time.Clock()                           #game clock
 WindowName=pygame.display.set_caption('game')
-# WindowLogo=pygame.display.set_icon()
-screen=pygame.display.set_mode(WindowSize,0,32)
-playerimage=pygame.image.load('1.png')
+running=True
+WindowSize=(600,400)
+displaySize=(300,200)
+window=pygame.display.set_mode(WindowSize,0,32)
+display=pygame.Surface(displaySize)
+
 movingRight=False
 movingLeft=False
 playerLocation=[50,50]
 playerYmomentum=0
+
+tilemap=[
+        [],
+        [],
+        [],
+        []
+]
+
 #main loop
 while running:        
-    screen.fill((0,0,0))
-    screen.blit(playerimage,playerLocation)                              #game loop    
+    display.fill((0,0,0))
+    display.blit(playerimage,playerLocation)                              #game loop    
+    
     #movement thing
     if movingRight==True:  
         playerLocation[0]+=4
     if movingLeft==True: 
         playerLocation[0]-=4
     
-    #falling down thing
-    if playerLocation[1]>WindowSize[1]-playerimage.get_height():
-        playerYmomentum=-playerYmomentum
-    if playerLocation[1]<WindowSize[1]-playerimage.get_height():
-        playerYmomentum+=1
-    playerLocation[1]+=playerYmomentum
+    # #falling down thing
+    # if playerLocation[1]>WindowSize[1]-playerimage.get_height():
+    #     playerYmomentum=-playerYmomentum
+    # if playerLocation[1]<WindowSize[1]-playerimage.get_height():
+    #     playerYmomentum+=1
+    # playerLocation[1]+=playerYmomentum
 
     #event loop    
     for event in pygame.event.get():
@@ -46,5 +64,8 @@ while running:
                 movingRight=True
             if event.key==pygame.K_LEFT:
                 movingLeft=True
-    clock.tick(60)
+    
+    window.blit(pygame.transform.scale(display,WindowSize),(0,0))
+    pygame.display.set_icon(WindowLogo)
     pygame.display.update()
+    clock.tick(60)
